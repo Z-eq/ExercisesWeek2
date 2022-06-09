c07f213a08451dbf470e23500a865fa721a01c6b
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-09 12:24:24 +0200

    Sort of array strings!

---
+++ src/main/java/se/lexicon/exercises/Quest3.java
@@ -0,0 +1,14 @@
+package se.lexicon.exercises;
+
+import java.util.Arrays;
+
+public class Quest3 {
+
+    public static void main(String[] args) {
+
+        String[] citys = { "Paris", "London", "New York" , "Stockholm"};
+        Arrays.sort(citys);
+        System.out.println(Arrays.toString(citys));
+
+    }
+}
