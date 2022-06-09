64770755a8fae0dcaee2fbad8f1848dc7a29db76
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-09 12:23:26 +0200

    Arrays store element

---
+++ src/main/java/se/lexicon/exercises/Quest1.java
@@ -0,0 +1,17 @@
+package se.lexicon.exercises;
+
+public class Quest1 {
+    public static void main(String[] args) {
+
+        int[] quest1 = {11 ,23 ,39, 45};
+
+        for (int i : quest1) {
+
+            //     for (int i = 0; i <quest1.length; i++) {
+            // below sout  works but is not a suitable way to learn coding!:)
+            //  System.out.print(first[0] + " " +first[1] + " "+ first[2]);
+
+            System.out.print(i + " ");
+        }
+    }
+}
