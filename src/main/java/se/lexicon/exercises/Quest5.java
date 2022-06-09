b8a0bd0cd5e025602e08fa433b16af33d7b130d2
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-09 21:52:57 +0200

    Simple 2-Dimensional arrays

---
+++ src/main/java/se/lexicon/exercises/Quest5.java
@@ -0,0 +1,16 @@
+package se.lexicon.exercises;
+
+
+public class Quest5 {
+
+    public static void main(String[] args) {
+        String[][] capitals =  {{"France " , "Paris " } , {"Sweden " , "Stockholm " }};
+
+
+        System.out.println(capitals[0][0] + capitals[0][1]);
+
+
+       System.out.println(capitals[1][0] + capitals[1][1]);
+    }
+}
+
