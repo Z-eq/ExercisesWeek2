fafe99f9d33d2186a046754369e7a678286e92f7
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-10 08:03:03 +0200

    Delete duplicate values in arrays

---
+++ src/main/java/se/lexicon/exercises/Quest8.java
@@ -0,0 +1,26 @@
+package se.lexicon.exercises;
+import java.util.Arrays;
+public class Quest8 {
+
+    public static void main(String[] args) {
+        int[] delDuplicate = {1, 3, 3, 4, 2, 1, 5, 6, 7, 7, 8, 10};
+
+        Arrays.sort(delDuplicate);
+        int j = 0;
+
+        for (int i = 0; i < delDuplicate.length - 1; i++)
+            if (delDuplicate[i] != delDuplicate[i + 1]) {
+                delDuplicate[j] = delDuplicate[i];
+                j++;
+            }
+
+        System.out.print("Arrays without duplicated value: ");
+        delDuplicate[j] = delDuplicate[delDuplicate.length - 1];
+        for (int i = 0; i <= j; i++) {
+
+            System.out.print(delDuplicate[i] + " ");
+
+        }
+
+    }
+}
